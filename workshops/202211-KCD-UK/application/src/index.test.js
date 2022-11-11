010cfad54e85ae181fe9340d3800b84e5f69f5cb
Federico Maggi <federico.maggi@mia-platform.eu>
2022-11-11 19:48:35 +0100

    added inventory placeholder tests

--- workshops/202211-KCD-UK/application/src/index.test.js
+++ workshops/202211-KCD-UK/application/src/index.test.js
@@ -23,8 +23,41 @@ tap.test('store api', async t => {
   })
 
   t.equal(response.statusCode, 200)
-  t.match(JSON.parse(response.payload), {
+  t.strictSame(JSON.parse(response.payload), {
     storeName: 'KCD Shop',
     address: 'CodeNode, 10 South Pl, London EC2M 7EB, UK',
   })
+})
+
+tap.test('get inventory api', async t => {
+  const service = await launchService({})
+  t.teardown(async() => {
+    await service.close()
+  })
+
+  const response = await service.inject({
+    method: 'GET',
+    url: `/inventory`,
+  })
+
+  t.equal(response.statusCode, 200)
+})
+
+tap.test('post inventory api', async t => {
+  const service = await launchService({})
+  t.teardown(async() => {
+    await service.close()
+  })
+
+  const response = await service.inject({
+    method: 'POST',
+    url: `/inventory`,
+    payload: {
+      name: 'new item',
+      sku: 13,
+      price: 12,
+    },
+  })
+
+  t.equal(response.statusCode, 200)
 })
