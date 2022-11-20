dbe564afdc4c710b983701d193f72556cb430e49
Federico Maggi <federico.maggi@mia-platform.eu>
2022-11-20 18:05:10 +0100

    fix: k8s config for running a simple mongodb

--- workshops/202211-KCD-UK/kubefiles/boot.sh
+++ workshops/202211-KCD-UK/kubefiles/boot.sh
@@ -4,3 +4,5 @@ kubectl apply -f ./kubefiles/routes.configmap.yaml
 kubectl apply -f ./kubefiles/policies.configmap.yaml
 kubectl apply -f ./kubefiles/application.service.yaml
 kubectl apply -f ./kubefiles/application.deployment.yaml
+kubectl apply -f ./kubefiles/mongodb.deployment.yaml
+kubectl apply -f ./kubefiles/mongodb.service.yaml
