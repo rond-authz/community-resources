a5d346e6de2e8a815dfecace1ebd45b6d7a0e8c2
Federico Maggi <federico.maggi@mia-platform.eu>
2022-11-20 23:33:08 +0100

    add: postman collection and improved readme

--- workshops/202211-KCD-UK/application/src/index.js
+++ workshops/202211-KCD-UK/application/src/index.js
@@ -7,11 +7,6 @@ const mongodb = require('@fastify/mongodb')
 const store = require('./api/store')
 const inventory = require('./api/inventory')
 
-// eslint-disable-next-line
-// admin_jwt: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiZXRoIiwibmFtZSI6IkJldGggU21pdGgiLCJpYXQiOjE1MTYyMzkwMjIsInJvbGUiOiJhZG1pbiJ9.M_Fe4mtcHCDtmd1CEnPgGo2cY-oXGPBXG4RJAUKNlS4"
-// eslint-disable-next-line
-// user_jet: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiZXRoIiwibmFtZSI6IkplcnJ5IFNtaXRoIiwiaWF0IjoxNTE2MjM5MDIyLCJyb2xlIjoidXNlciJ9.LjI6XBWM0z94eUP0NLiRqlXPSzorsOnJ7J8jPfN-JNc"
-
 const envSchema = {
   type: 'object',
   properties: {
